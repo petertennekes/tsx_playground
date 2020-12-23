170d026034f11a6c52f4a836b219a1d803e42f28
Peter Tennekes <petert.tennekes@teliacompany.com>
2020-12-23 13:05:37 +0200

    Add reverse side to card

--- src/playinCards.tsx
+++ src/playinCards.tsx
@@ -1,13 +1,40 @@
+import Card from 'react-bootstrap/Card'
+import ReactMarkdown from 'react-markdown'
+
+import 'holderjs'
+import { useState } from 'react'
+enum CardSides {Front, Reverse}
+
 interface CardProps  {
     title :string;
+    description: string;
+    backgroundText: string;
 };
 
 function PlayingCard(props: CardProps) {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-        </div>
-    )
+    const [visibleSide, setVisibleSide] = useState(CardSides.Front);
+
+
+    if (visibleSide === CardSides.Reverse){
+        return (
+            <Card style={{ width: '18rem' }} onClick={()=> setVisibleSide(CardSides.Front)}>
+                <Card.Body>
+                    <Card.Text><ReactMarkdown>{props.backgroundText}</ReactMarkdown></Card.Text>
+                </Card.Body>
+            </Card>
+        )
+   } else {
+        return (
+            <Card style={{ width: '18rem' }} onClick={()=> setVisibleSide(CardSides.Reverse)}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title><h1>{props.title}</h1></Card.Title>
+                    <Card.Text>{props.description}</Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    }
 }
+
 
 export default PlayingCard
