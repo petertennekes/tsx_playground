b5e553aaa075cc621add2fb107a5fc776d420d48
Peter Tennekes <petert.tennekes@teliacompany.com>
2020-12-23 10:47:08 +0200

    Add playingcard to ui

--- src/playinCards.tsx
+++ src/playinCards.tsx
@@ -1,3 +1,13 @@
-function PlayCard(props) {
-    
+interface CardProps  {
+    title :string;
+};
+
+function PlayingCard(props: CardProps) {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+        </div>
+    )
 }
+
+export default PlayingCard
