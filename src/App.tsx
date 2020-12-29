d02d960896ae932bd76b9ac7840129e86cfe8393
Peter Tennekes <petert.tennekes@teliacompany.com>
2020-12-29 13:47:35 +0200

    Renamed class, add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import PlayingCard from './playinCards'
+import PlayingCard from './PlayingCards'
 
 function App() {
   return (
     <div className="App">
-      <PlayingCard title="test" description="short description of the title. max two lines." 
-      backgroundText="*Test* markdowned text. [I'm an inline-style link](https://www.google.com)"/>
+      <div className="container">
+        <PlayingCard title="test" description="short description of the title. max two lines." 
+        backgroundText="*Test* markdowned text. [I'm an inline-style link](https://www.google.com)"/>
+        </div>
     </div>
   );
 }
--- src/PlayingCards.tsx
+++ src/PlayingCards.tsx
@@ -1,8 +1,10 @@
 import Card from 'react-bootstrap/Card'
 import ReactMarkdown from 'react-markdown'
-
-import 'holderjs'
+import './PlayingCards.css'
 import { useState } from 'react'
+
+
+
 enum CardSides {Front, Reverse}
 
 interface CardProps  {
@@ -17,16 +19,16 @@ function PlayingCard(props: CardProps) {
 
     if (visibleSide === CardSides.Reverse){
         return (
-            <Card style={{ width: '18rem' }} onClick={()=> setVisibleSide(CardSides.Front)}>
-                <Card.Body>
-                    <Card.Text><ReactMarkdown>{props.backgroundText}</ReactMarkdown></Card.Text>
-                </Card.Body>
-            </Card>
+                <Card className="playingCard" onClick={()=> setVisibleSide(CardSides.Front)}>
+                    <Card.Body>
+                        <Card.Text><ReactMarkdown>{props.backgroundText}</ReactMarkdown></Card.Text>
+                    </Card.Body>
+                </Card>
         )
    } else {
         return (
-            <Card style={{ width: '18rem' }} onClick={()=> setVisibleSide(CardSides.Reverse)}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card className="playingCard" onClick={()=> setVisibleSide(CardSides.Reverse)}>
+                <Card.Img variant="top" src="logo192.png" />
                 <Card.Body>
                     <Card.Title><h1>{props.title}</h1></Card.Title>
                     <Card.Text>{props.description}</Card.Text>
@@ -37,4 +39,4 @@ function PlayingCard(props: CardProps) {
 }
 
 
-export default PlayingCard
+export default PlayingCard 
