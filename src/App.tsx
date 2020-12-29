1475fb8d80fc87d7d0f5d82a450afe40fdaeb136
Peter Tennekes <petert.tennekes@teliacompany.com>
2020-12-29 16:10:57 +0200

    Refactor PlayingCards

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import './App.css';
 import PlayingCard from './PlayingCards'
 
 function App() {
+  const cards = [
+    {
+      title: "one", description: "card 1", detailedText: "it is card 1"
+    },
+    {
+      title: "two", description: "card 2", detailedText: "it is card 2"
+    },
+    {
+      title: "three", description: "card 3", detailedText: "it is card 3"
+    },
+    {
+      title: "foue", description: "card 4", detailedText: "it is card 4"
+    }
+  ]
   return (
     <div className="App">
-      <div className="container">
-        <PlayingCard title="test" description="short description of the title. max two lines." 
-        backgroundText="*Test* markdowned text. [I'm an inline-style link](https://www.google.com)"/>
-        </div>
+      <div style={{ display: 'flex' }}>
+        {cards.map((card, index) => <PlayingCard {...card} />)}
+
+      </div>
     </div>
   );
 }
--- src/PlayingCards.tsx
+++ src/PlayingCards.tsx
@@ -3,40 +3,35 @@ import ReactMarkdown from 'react-markdown'
 import './PlayingCards.css'
 import { useState } from 'react'
 
+enum CardSides { Front, Reverse }
 
-
-enum CardSides {Front, Reverse}
-
-interface CardProps  {
-    title :string;
+interface CardProps {
+    title: string;
     description: string;
-    backgroundText: string;
+    detailedText: string;
+    imgSrc?: string;
 };
 
 function PlayingCard(props: CardProps) {
     const [visibleSide, setVisibleSide] = useState(CardSides.Front);
-
-
-    if (visibleSide === CardSides.Reverse){
-        return (
-                <Card className="playingCard" onClick={()=> setVisibleSide(CardSides.Front)}>
-                    <Card.Body>
-                        <Card.Text><ReactMarkdown>{props.backgroundText}</ReactMarkdown></Card.Text>
-                    </Card.Body>
-                </Card>
-        )
-   } else {
-        return (
-            <Card className="playingCard" onClick={()=> setVisibleSide(CardSides.Reverse)}>
-                <Card.Img variant="top" src="logo192.png" />
-                <Card.Body>
-                    <Card.Title><h1>{props.title}</h1></Card.Title>
-                    <Card.Text>{props.description}</Card.Text>
-                </Card.Body>
-            </Card>
-        )
+    const toggleSide = () => {
+        if (visibleSide === CardSides.Front) {
+            setVisibleSide(CardSides.Reverse)
+        } else {
+            setVisibleSide(CardSides.Front)
+        }
     }
-}
+    const { title, description, detailedText, imgSrc = "logo192.png" } = props
 
+    return (<Card className="playingCard" onClick={toggleSide}>
+        <Card.Img variant="top" src={imgSrc} />
+        <Card.Body>
+            {visibleSide === CardSides.Front && <Card.Title><h1>{title}</h1></Card.Title>}
+            <Card.Text>
+            {visibleSide === CardSides.Front ? description : <ReactMarkdown>{detailedText}</ReactMarkdown>}
+            </Card.Text>
+        </Card.Body>
+    </Card >)
+}
 
 export default PlayingCard 
