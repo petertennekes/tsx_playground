2a77aa5322b4982a309568e6685d3118a8b481a2
Peter Tennekes <petertennekes@gmail.com>
2021-02-19 16:18:00 +0200

    Create websocket to share game state

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+
 const path = require('path');
 const app = express();
+
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+ // ...
+});
+
+io.on("connection", (socket) => {
+ // ...
+ console.log(socket.id, " connected");
+ socket.on("cards", (data)=>{
+  console.log(socket.id, " received cards");
+
+  socket.broadcast.emit("cards", data)
+  console.log(socket.id, " server sending cards");
+ });
+});
+
+
+/*
+
+const wss = new WebSocket.Server({ port: 8081 });
+
+wss.on('connection', function connection(ws) {
+ console.log("connected");
+ ws.on('message', function incoming(data) {
+  console.log('received data. length: %s', data.length);
+
+  wss.clients.forEach(function each(client) {
+   if (client !== ws && client.readyState === WebSocket.OPEN) {
+    client.send(data);
+   }
+  });
+ });
+});
+
+*/
 app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/api/cards', function (req, res) {
+ console.log("Req file");
  res.sendFile(path.join(__dirname, 'public', 'card-deck.json') )
 });
 
@@ -13,4 +50,8 @@ app.get('/', (req, res) => {
  res.send('Hello World!')
 });
 
-app.listen(process.env.PORT || 8080);
+
+httpServer.listen(8080);
+
+console.log("server")
+//app.listen(process.env.PORT || 8080);
--- src/useCards.tsx
+++ src/useCards.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { CardProps } from "./PlayingCard";
+import { io } from "socket.io-client";
 
 const fetchJSON = (url: string) => fetch(url).then((r) => r.json());
+const socket = io();
+console.log("connected: ", socket.id);
 
 export default function useCards(cardsURL: string) {
   const [loading, setLoading] = useState(true);
+  const [incoming, setIncoming] = useState(false);
   const [cards, setCards] = useState<CardProps[]>([]);
 
   const createOnDrop = (indexTarget: number) => {
@@ -24,6 +28,20 @@ export default function useCards(cardsURL: string) {
       setLoading(false);
     });
   }, [cardsURL]);
+
+  useEffect(()=>{
+    if (!incoming){ //looks a bit ugly solution. problem we don't want to emit the changed cards if the change is a result of a recevied card deck
+      socket.emit("cards", JSON.stringify(cards));
+    }
+    }, [cards])
+
+  useEffect(() => {
+    socket.on('cards', (payload: string)=> {
+      setIncoming(true);
+      setCards(JSON.parse(payload));
+      setIncoming(false);
+    });
+  }, []);
 
   return {
     cards,
