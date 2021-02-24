d099dc8e9c4615ec3ed29eb2cb9a6ecdf7d16cca
Peter Tennekes <petertennekes@gmail.com>
2021-02-24 16:51:21 +0200

    store cards for new joiners remove unneeded code

--- server.js
+++ server.js
@@ -6,46 +6,42 @@ const fs = require('fs');
 
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
- // ...
+    // ...
 });
+const gamestore = {}
+
+const createNewGame = () => {
+  const cardData = fs.readFileSync(filename);
+  const gameId = Math.random().toString(36).substring(7)
+  gamestore[gameId] = JSON.parse(cardData);
+  return gameId
+}
 
 const filename = path.join(__dirname, 'public', 'card-deck.json');
-const cardData = fs.readFileSync(filename);
 io.on("connection", (socket) => {
- // ...
- console.log(socket.id, " connected");
- const game_id = socket.handshake.query.game_id;
- console.log(game_id);
- if (socket.handshake.query.game_id){
-   socket.join(socket.handshake.query.game_id);
-   socket.emit("cards", JSON.parse(cardData))
- }
- socket.on("cards", (data)=>{
-  console.log(socket.id, " received cards");
-
-  io.to(game_id).emit("cards", data)
-  console.log(io.in(game_id).allSockets(), " server sending cards");
- });
+    // ...
+    console.log(socket.id, " connected");
+    const game_id = socket.handshake.query.game_id;
+    console.log(game_id);
+    if (socket.handshake.query.game_id) {
+        socket.join(socket.handshake.query.game_id);
+        socket.emit("cards", gamestore[game_id])
+    }
+    socket.on("cards", (data) => {
+        console.log(socket.id, " received cards");
+        gamestore[game_id] = data;
+        io.to(game_id).emit("cards", data)
+        console.log(io.in(game_id).allSockets(), " server sending cards");
+    });
 });
 
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/api/new_game', function (req, res) {
- console.log("new game started");
- res.send({"game_id": Math.random().toString(36).substring(7) })
+    console.log("new game started");
+    res.send({"game_id": createNewGame()})
 })
-
-app.get('/api/cards', function (req, res) {
- console.log("Req file");
- res.sendFile(filename)
-});
-
-app.get('/', (req, res) => {
- res.send('Hello World!')
-});
-
 
 httpServer.listen(8080);
 
 console.log("server")
-//app.listen(process.env.PORT || 8080);
